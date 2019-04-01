46d5f02caf7a0467b2751040bd24c1e4aca12761
Falitto <pavelkruchkov@mail.ru>
2019-04-01 12:25:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from werkzeug.utils import secure_filename
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:9935@localhost:3306/reportdb?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:9935@localhost:3306/reportdb?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/uploads/'
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx'])
