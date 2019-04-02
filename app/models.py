694dd24a7331c1c68607f6b3948a8e725265738b
Falitto <pavelkruchkov@mail.ru>
2019-04-02 15:05:54 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(50), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    orgnization = db.Column(db.Integer, db.ForeignKey('organizations.id'))
+    organization = db.Column(db.Integer, db.ForeignKey('organizations.id'))
     subdivision = db.Column(db.Integer, db.ForeignKey('subdivisions.id'))
     first_name = db.Column(db.String(50))
     patronymic = db.Column(db.String(50))
